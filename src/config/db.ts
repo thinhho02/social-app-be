f866b8d6c00961b0ac5985f61d089aa3155618b0
thinhho02 <thinhhocm02@gmail.com>
2025-01-30 21:36:14 +0700

    i

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async (): Promise<void> => {
   try {
-    const uri = process.env.MONGODB_URI || "";
-    if (uri == "") {
-      throw new Error("Error connecting to MongoDB")
-    }
+    // const uri = process.env.MONGODB_URI || "";
+    // if (uri == "") {
+    //   throw new Error("Error connecting to MongoDB")
+    // }
     await mongoose.connect("mongodb+srv://vercel-admin-user:1SnsFe1Qi3jP1pLK@learn1.kd9vc1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
     console.log("MongoDB connected!");
   } catch (error) {
