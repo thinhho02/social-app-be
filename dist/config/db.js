02988a76eb8afe52bfc0d052da26abe33e41ac16
thinhho02 <thinhhocm02@gmail.com>
2025-01-30 19:11:58 +0700

    c

--- dist/config/db.js
+++ dist/config/db.js
@@ -1,21 +1,26 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const connectDB = async () => {
-    try {
-        const uri = process.env.MONGO_CONNECTION_DB || "";
-        if (uri == "") {
-            throw new Error("Error connecting to MongoDB");
-        }
-        await mongoose_1.default.connect(uri);
-        console.log("MongoDB connected!");
+  try {
+    const uri = process.env.MONGO_CONNECTION_DB || "";
+    if (uri == "") {
+      throw new Error("Error connecting to MongoDB");
     }
-    catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);
-    }
+    await mongoose_1.default.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+    });
+    console.log("MongoDB connected!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
 };
 exports.default = connectDB;
