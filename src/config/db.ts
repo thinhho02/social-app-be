42dcc8f040cce8e0f1d89e0efec866d3ea5dcc93
thinhho02 <thinhhocm02@gmail.com>
2025-01-30 21:30:57 +0700

    k

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,7 @@ const connectDB = async (): Promise<void> => {
     if (uri == "") {
       throw new Error("Error connecting to MongoDB")
     }
-    await mongoose.connect('mongodb+srv://vercel-admin-user:1SnsFe1Qi3jP1pLK@learn1.kd9vc1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+    await mongoose.connect("mongodb+srv://vercel-admin-user:1SnsFe1Qi3jP1pLK@learn1.kd9vc1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
     console.log("MongoDB connected!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
