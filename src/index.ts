cf6261db91f8ba63c7155c09fd8365f1ad425950
thinhho02 <thinhhocm02@gmail.com>
2025-02-01 21:05:00 +0700

    testerror

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,16 @@ import connectDB from "./config/db";
 import creatorRoute from "./routes/creator.route";
 import categoryRoute from "./routes/category.route";
 import tagRoute from "./routes/tag.route";
+import mongoose from "mongoose";
 
 
 const port = process.env.PORT || 3001
 const app = express();
-
+const uri = process.env.MONGODB_URI || "";
+if (uri == "") {
+    throw new Error("Error connecting to MongoDB")
+}
+mongoose.connect(uri).catch(error => console.log(error))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,13 +25,9 @@ app.use(cors({
     origin: process.env.ORIGIN_PATH_FRONTEND || "*",
     credentials: true
 }))
-app.listen(port, async () => {
-    console.log(`server running listen port ${port}`);
-    await connectDB()
-})
 
 app.get("/", (req, res) => {
-   res.send("connect")
+    res.send("connect")
 })
 
 // routers
@@ -36,6 +37,10 @@ app.use("/tag", tagRoute);
 
 // middleware error handler
 app.use(errorHandler)
+app.listen(port, async () => {
+    console.log(`server running listen port ${port}`);
+    await connectDB()
+})
 
 
 export default app
