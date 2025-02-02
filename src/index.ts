11daaddc80ee97c49613eb174fce04ebf6810a81
thinhho02 <thinhhocm02@gmail.com>
2025-02-02 11:13:11 +0700

    jjjj

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.ORIGIN_PATH_FRONTEND || "*",
+    origin: "*",
     credentials: true
 }))
 
