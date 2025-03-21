e8a552d709482c4f2a7c3a9d2b560a9e4b2414ec
thinhho02 <thinhhocm02@gmail.com>
2025-03-21 23:47:31 +0700

    change cors 1

--- src/index.ts
+++ src/index.ts
@@ -26,13 +26,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: "*",
     credentials: true
 }));
 
